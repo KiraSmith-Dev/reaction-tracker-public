2c87c5753bb122d266afc7cc260b7e440c2ce18f
int3nse-git <int3nse-git@users.noreply.github.com>
2021-09-07 21:09:13 -0400

    Add config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const token = '-token-censored-';
+
+const colors = {
+    success: 0x2dfc9f,
+    error: 0xfc2d2d
+}
+
+const prefix = './';
+
+const weeklyAmount = 30;
+
+module.exports = { token, colors, prefix, weeklyAmount }
